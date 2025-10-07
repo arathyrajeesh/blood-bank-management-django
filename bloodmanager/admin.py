8c7a8870ff751cb0ad23f4981c99d0d7992f1d0e
Arathy <arathyrajeesh2@gmail.com>
2025-10-07 10:05:19 +0530

    models register

--- bloodmanager/admin.py
+++ bloodmanager/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Donor, Patient
+from .models import Donor, Patient,BloodStock
 
 admin.site.register(Donor)
 admin.site.register(Patient)
-
+admin.site.register(BloodStock)
